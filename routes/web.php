32844570425ecca64071b8b521b75e901d419843
damianolagana <damianolagana@gmail.com>
2023-06-29 10:16:32 +0200

    create model and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $links = config('store.someLinks');
-    return view('welcome', compact('links'));
-});
+Route::get('/',[PageController::class,'index']);
 
 Route::get('/otherpage', function () {
     $links = config('store.someLinks');
